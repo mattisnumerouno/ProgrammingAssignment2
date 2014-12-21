60af0ccc24dbefd133c7290e7fb0e60fb7316ac3
mattisnumerouno <mlanghornes@gmail.com>
2014-12-21 13:19:44 -0600

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions solves for the inverse of a matrix and then caches the value using superassignment.
+## This value is then returned using the second function
 
-## Write a short comment describing this function
+## This function sets the objects for the cachesolve function and handles superassignment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  get <- function() {x}
+  setinv <-function(inverse) {i <<- inverse}
+  getinv <- function() {i}
+  list( get = get,
+        setinv = setinv,
+        getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function returns a cached value if it's there, and solves for the inverse of object x if it isn't
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Returns a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)) {
+      message("getting cached data")
+      return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    i
 }
